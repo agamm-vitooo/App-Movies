6485e5e19dcc2ce2f7c3b551a3e12cff9beafa18
agam <agamvito7@gmail.com>
2023-12-02 22:43:37 +0700

    second commit

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,21 +1,33 @@
+import React, { useState } from 'react';
 
 const NavigationBar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className="sticky top-0">
       <nav className="bg-slate-800">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-              <img width="30" height="30" src="https://img.icons8.com/officel/16/film-reel.png" alt="film-reel"/>
-              <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">Movies</span>
+            <img width="30" height="30" src="https://img.icons8.com/officel/16/film-reel.png" alt="film-reel"/>
+            <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">Movies</span>
           </a>
-          <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-              <span className="sr-only">Open main menu</span>
-              <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
-              </svg>
-
+          <button
+            onClick={toggleMenu}
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-default"
+            aria-expanded={isMenuOpen}
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
+            </svg>
           </button>
-          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+          <div className={`w-full md:block md:w-auto ${isMenuOpen ? 'block' : 'hidden'}`} id="navbar-default">
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0">
               <li>
                 <a href="#" className="block py-2 px-3 text-white" aria-current="page">Home</a>
@@ -31,7 +43,7 @@ const NavigationBar = () => {
         </div>
       </nav>
     </div>
-  )
-}
+  );
+};
 
-export default NavigationBar
+export default NavigationBar;
